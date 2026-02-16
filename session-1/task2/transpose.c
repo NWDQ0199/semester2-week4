793d703b0439b45e9fd86b5ee17023f1fbd2137d
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-02-16 12:07:24 +0000

    S2W4S1 task2 part 3

--- session-1/task2/transpose.c
+++ session-1/task2/transpose.c
@@ -3,16 +3,69 @@
  * Transpose of a matrix
  */
 
- #include <stdio.h>
+#include <stdio.h>
 
- int main( void ) {
+int main(void)
+{
     int a[4][4];
-
     /*
     Intialise the matrix a to 2i-j.
     Print out matrix a. 
     Write code for the transpose - you can use other variables as necessary but not a cpoy of the matrix 
     Print the transpose.
     */
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            a[i][j]=2*i-j;
+        }
+    }
+    //PrintIntMatrix(&a,4,4);
+    for(int i=0;i<4;i++)
+    {
+        printf("[");
+        for(int j=0;j<4;j++)
+        {
+            if(j==3) printf("%i",a[i][j]);
+            else printf("%i ",a[i][j]);
+        }
+        printf("]\n");
+    }
+    printf("\n");
+    int b[4][4];
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            b[i][j]=a[j][i];
+        }
+    }
+    //PrintIntMatrix(&b,4,4);
+    for(int i=0;i<4;i++)
+    {
+        printf("[");
+        for(int j=0;j<4;j++)
+        {
+            if(j==3) printf("%i",b[i][j]);
+            else printf("%i ",b[i][j]);
+        }
+        printf("]\n");
+    }
     return 0;
- }
+}
+
+/*int PrintIntMatrix(int[][] *ptr, int rows, int columns)
+{
+    arr=*ptr;
+    for(int i=0;i<rows;i++)
+    {
+        printf("[");
+        for(int j=0;j<columns;j++)
+        {
+            if(j==columns-1) printf("%i",arr[i][j]);
+            else printf("%i ",arr[i][j]);
+        }
+        printf("]\n");
+    }
+}*/
