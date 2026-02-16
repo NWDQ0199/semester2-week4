b6dec1c82c5dd717f5213393e17d21682809a2df
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-02-16 11:44:48 +0000

    S2W4S1 task2 part 1

--- session-1/task2/matrix_add.c
+++ session-1/task2/matrix_add.c
@@ -1,20 +1,34 @@
 
 /*
  * Sum of 2 matrices
- */
+*/
 
- #include <stdio.h>
+#include <stdio.h>
 
- int main( void ) {
+int main(void)
+{
     float a[4][4];
     float b[4][4];
     float c[4][4];
-
     /*
-    Intialise the matrix a and b entries to 1. 
+    Intialise the matrix a and b entries to 1.
     Write code to compute the sum.
     Store your answer in matrix c
     Print your final answer
     */
+    for(int i=0;i<4;i++)
+    {
+        printf("[");
+        for(int j=0;j<4;j++)
+        {
+            //i is row and j is column
+            a[i][j]=1;
+            b[i][j]=1;
+            c[i][j]=a[i][j]+b[i][j];
+            if(j==3) printf("%f",c[i][j]);
+            else printf("%f ",c[i][j]);
+        }
+        printf("]\n");
+    }
     return 0;
- }
+}
