3334cc9feba2dee324d5ba6601eabd0507604dab
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-02-16 11:20:28 +0000

    S2W4S1 task1 part 1

--- session-1/task1/factorial.c
+++ session-1/task1/factorial.c
@@ -7,8 +7,15 @@
 
 int main(void)
 {
-    int f[20];
-
+    int fLen=20;
+    int f[fLen];
+    f[0]=1;
+    for(int i=1;i<fLen;i++)
+    {
+        f[i]=i*f[i-1];
+        if(i!=fLen-1) printf("%i, ",f[i]);
+        else printf("%i\n",f[i]);
+    }
     /*
     Code to compute the factorial of each array index
     Print your final answer
