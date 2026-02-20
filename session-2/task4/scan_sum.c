756889f7f4024978f26ba1096bbf860e9e4271f2
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-02-20 10:40:36 +0000

    S2W4S2 task 4

--- session-2/task4/scan_sum.c
+++ session-2/task4/scan_sum.c
@@ -5,15 +5,26 @@ int main(void)
 {
 
     // define suitable data
+    // use scanf to read from the terminal
+    // print the output from scanf and the data values
+
+    /*
     int a;
     int b;
-    // use scanf to read from the terminal
-    int count=scanf("%i + %i =",&a,&b);
-    /*char str[100];
-    fgets(str,100,stdin);
-    int count=sscanf(str,"%99s",str);*/
-    // print the output from scanf and the data values 
-    printf(" %i\n",a+b);
+    int count=scanf("%i %i",&a,&b);
+    printf("%i+%i=%i\n",a,b,a+b);
     printf("Number of items scanned: %i\n",count);
     return 0;
+    //*/
+
+    //*
+    int a;
+    int b;
+    char str[100];
+    fgets(str,100,stdin);
+    int count=sscanf(str,"%i %i",&a,&b);
+    printf("%i+%i=%i\n",a,b,a+b);
+    printf("Number of items scanned: %i\n",count);
+    return 0;
+    //*/
 }
