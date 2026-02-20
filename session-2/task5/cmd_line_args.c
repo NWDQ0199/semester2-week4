31db3861f6f3fa33fed21096a4324b0141f04ae5
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-02-20 10:41:28 +0000

    S2W4S2 task 5

--- session-2/task5/cmd_line_args.c
+++ session-2/task5/cmd_line_args.c
@@ -7,11 +7,13 @@
 
 #include <stdio.h>
 
-int main( int argc, char **argv ) {
+int main(int argc, char **argv)
+{
 
     printf("Number of args: %d\n",argc);
 
-    for( int k=0; k<argc; ++k ) {
+    for(int k=0;k<argc;k++)
+    {
         printf("argv[%d]: %s\n",k,argv[k]);
     }
     return 0;
