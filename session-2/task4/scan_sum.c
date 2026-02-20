038030a2b4bcb53b61ff0fab98af55149235cd72
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-02-20 10:30:56 +0000

    S2W4S2 task 4

--- session-2/task4/scan_sum.c
+++ session-2/task4/scan_sum.c
@@ -1,12 +1,19 @@
 
 #include <stdio.h>
 
-int main( void ) {
+int main(void)
+{
 
     // define suitable data
-
+    int a;
+    int b;
     // use scanf to read from the terminal
+    int count=scanf("%i + %i =",&a,&b);
+    /*char str[100];
+    fgets(str,100,stdin);
+    int count=sscanf(str,"%99s",str);*/
     // print the output from scanf and the data values 
-
+    printf(" %i\n",a+b);
+    printf("Number of items scanned: %i\n",count);
     return 0;
 }
